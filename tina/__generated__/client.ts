1e74121583b3559caee6b89fc9aae76000ce1f33
Sana0523 <sanafathimanadaf23@gmail.com>
2026-05-19 12:03:33 +0530

    few more changes to fix tina

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: 'C:/Users/sanaf/self-v1-main/self-v1-main/tina/__generated__/.cache/1779139469737', url: 'https://content.tinajs.io/2.4/content/158c837e-2cdf-46d6-af23-e307e7d0d829/github/main', token: 'c27498201ee01e83d06d63dcc179f8a159d3aab1', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'c27498201ee01e83d06d63dcc179f8a159d3aab1', queries,  });
 export default client;
   
